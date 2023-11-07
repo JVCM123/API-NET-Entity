aebeb5e3484e7a200ed1e5f115f7947c4a484358
JVCM <jvcmoura2012@gmail.com>
2023-11-07 20:53:50 -0300

    comentarios de documentação swagger

--- AwesomeDevEvents.API/Controllers/DevEventsController.cs
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -16,14 +16,30 @@ namespace AwesomeDevEvents.API.Controllers
             _dbContext = context;
         }
 
+
+        /// <summary>
+        /// Obter todos eventos cadastrados no banco
+        /// </summary>
+        /// <returns>Coleção de Eventos</returns>
+        /// <response code="200">Sucesso</response>
         [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         public IActionResult GetAll()
         {
             var devEvents = _dbContext.DevEvents.Where(e => !e.IsDeleted).ToList();
             return Ok(devEvents);
         }
 
+        /// <summary>
+        /// Obter um evento com base no ID
+        /// </summary>
+        /// <param name="id">Identificador único do evento</param>
+        /// <returns>Dados do evento</returns>
+        /// <responde code="200">Sucesso</responde>
+        /// <responde code="404">Não Encontrado</responde>
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult GetById(Guid id)
         {
             var devEvents = _dbContext.DevEvents
@@ -37,7 +53,17 @@ namespace AwesomeDevEvents.API.Controllers
             return Ok(devEvents);
         }
 
+        /// <summary>
+        /// Cadastra um Evento no banco de dados
+        /// </summary>
+        /// <remarks>
+        /// Exemplo de dados do Evento: {"id": "3fa85f64-5717-4562-b3fc-2c963f66afa6","title": "string","description": "string","startDate": "2023-11-07T23:45:57.579Z","endDate": "2023-11-07T23:45:57.579Z","isDeleted": true}
+        /// </remarks>
+        /// <param name="devEvent">Dados do evento a ser inserido</param>
+        /// <returns>Rota do objeto recém criado</returns>
+        /// <response code="201">Sucesso</response>
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         public IActionResult Post(DevEvent devEvent)
         {
             _dbContext.DevEvents.Add(devEvent);
@@ -45,7 +71,21 @@ namespace AwesomeDevEvents.API.Controllers
             return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
         }
 
+
+        /// <summary>
+        /// Atualizar um evento
+        /// </summary>
+        /// <remarks>
+        /// Exemplo de dados do Evento: {"id": "3fa85f64-5717-4562-b3fc-2c963f66afa6","title": "string","description": "string","startDate": "2023-11-07T23:45:57.579Z","endDate": "2023-11-07T23:45:57.579Z","isDeleted": true}
+        /// </remarks>
+        /// <param name="id">Identificador único do evento</param>
+        /// <param name="input">Novos dados do evento</param>
+        /// <returns>null</returns>
+        /// <response code="404">Evento não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Update(Guid id, DevEvent input)
         {
             var devEvents = _dbContext.DevEvents.SingleOrDefault(e => e.Id == id);
@@ -61,7 +101,16 @@ namespace AwesomeDevEvents.API.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Excluir um evento
+        /// </summary>
+        /// <param name="id">Identificador único do evento</param>
+        /// <returns>null</returns>
+        /// <response code="404">Evento não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult Delete(Guid id)
         {
             var devEvents = _dbContext.DevEvents.SingleOrDefault(e => e.Id == id);
@@ -77,7 +126,20 @@ namespace AwesomeDevEvents.API.Controllers
             return NoContent();
         }
 
+
+        /// <summary>
+        /// Cadastrar palestrante
+        /// </summary>
+        /// <remarks>
+        /// Exemplo de dados do palestrante: {"id": "3fa85f64-5717-4562-b3fc-2c963f66afa6","name": "string","talkTitle": "string","talkDescription": "string","linkedInProfile": "string","devEventId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"}</remarks>
+        /// <param name="id">Identificador único do evento</param>
+        /// <param name="speaker">Dados do palestrante a ser adicionado</param>
+        /// <returns>null</returns>
+        /// <response code="404">Evento não encontrado</response>
+        /// <response code="204">Sucesso</response>
         [HttpPost("{id}/speakers")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public IActionResult PostSpeaker(Guid id, DevEventSpeaker speaker)
         {
             speaker.DevEventId = id;
--- AwesomeDevEvents.API/Program.cs
+++ AwesomeDevEvents.API/Program.cs
@@ -1,5 +1,6 @@
 using AwesomeDevEvents.API.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,7 +13,19 @@ builder.Services.AddDbContext<DevEventDbContext>(o => o.UseSqlServer(connectionS
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Title = "AwesomeDevEvents.API",
+        Version = "v1",
+        Contact = new OpenApiContact() { Email = "jvcmoura2012@gmail.com", Name = "João" }
+    });
+
+    var xmlFile = "AwesomeDevEvents.API.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    s.IncludeXmlComments(xmlPath);
+});
 
 var app = builder.Build();
 
