d1ab37dc26a227d6a18942e3df796d12957c4570
JVCM <jvcmoura2012@gmail.com>
2023-11-07 20:20:53 -0300

    Commit inicial

---
+++ AwesomeDevEvents.API/Controllers/DevEventsController.cs
@@ -0,0 +1,98 @@
+﻿using AwesomeDevEvents.API.Entities;
+using AwesomeDevEvents.API.Persistence;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeDevEvents.API.Controllers
+{
+    [Route("api/DevEvents")]
+    [ApiController]
+    public class DevEventsController : ControllerBase
+    {
+        private readonly DevEventDbContext _dbContext;
+        public DevEventsController(DevEventDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var devEvents = _dbContext.DevEvents.Where(e => !e.IsDeleted).ToList();
+            return Ok(devEvents);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            var devEvents = _dbContext.DevEvents
+                .Include(de => de.Speakers)
+                .SingleOrDefault(e => e.Id == id);
+
+            if (devEvents == null)
+            {
+                return NotFound();
+            }
+            return Ok(devEvents);
+        }
+
+        [HttpPost]
+        public IActionResult Post(DevEvent devEvent)
+        {
+            _dbContext.DevEvents.Add(devEvent);
+            _dbContext.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = devEvent.Id }, devEvent);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(Guid id, DevEvent input)
+        {
+            var devEvents = _dbContext.DevEvents.SingleOrDefault(e => e.Id == id);
+            if (devEvents == null)
+            {
+                return NotFound();
+            }
+            devEvents.Update(input.Title, input.Description, input.StartDate, input.EndDate);
+
+            _dbContext.Update(devEvents);
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var devEvents = _dbContext.DevEvents.SingleOrDefault(e => e.Id == id);
+            if (devEvents == null)
+            {
+                return NotFound();
+            }
+
+            devEvents.Delete();
+
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPost("{id}/speakers")]
+        public IActionResult PostSpeaker(Guid id, DevEventSpeaker speaker)
+        {
+            speaker.DevEventId = id;
+
+            var devEvents = _dbContext.DevEvents.Any(e => e.Id == id);
+
+            if (!devEvents)
+            {
+                return NotFound();
+            }
+
+            _dbContext.devEventSpeakers.Add(speaker);
+            _dbContext.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
---
+++ AwesomeDevEvents.API/Persistence/DevEventDbContext.cs
@@ -0,0 +1,46 @@
+﻿using AwesomeDevEvents.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeDevEvents.API.Persistence
+{
+    public class DevEventDbContext : DbContext
+    {
+        public DevEventDbContext(DbContextOptions<DevEventDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<DevEvent> DevEvents { get; set; }
+        public DbSet<DevEventSpeaker> devEventSpeakers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DevEvent>(e =>
+            {
+                e.HasKey(de => de.Id);
+
+                e.Property(de => de.Title)
+                    .IsRequired(false);
+
+                e.Property(de => de.Description)
+                    .HasMaxLength(200)
+                    .HasColumnType("varchar(200)");
+
+                e.Property(de => de.StartDate)
+                    .HasColumnName("Start_Date");
+
+
+                e.Property(de => de.EndDate)
+                    .HasColumnName("End_Date");
+
+                e.HasMany(de => de.Speakers)
+                    .WithOne()
+                    .HasForeignKey(s => s.DevEventId);
+            });
+
+            modelBuilder.Entity<DevEventSpeaker>(e =>
+            {
+                e.HasKey(de => de.Id);
+            });
+        }
+    }
+}
